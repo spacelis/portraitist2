92d24db47638320635f1cb08f859a23037a83179
wenli <spacelis@gmail.com>
2014-08-22 00:08:47 +0200

    Add a setting for collection path

--- client/components/js/datasource.js
+++ client/components/js/datasource.js
@@ -17,22 +17,29 @@ define(['resource_ctrl', 'crossfilter', 'underscore', 'utils', 'lenz', 'jQuery',
         mapping: {name: 'mapping', label: 'Field Mapping', type: 'text', tip: 'A JSON string of object mapping the data.', value: '' }, 
         datetime: {name: 'datetime', label: 'Datetime Fields', type: 'text', tip: 'The fields that is of type datetime.', value: ''},
         format: {name: 'format', label: 'Format', type: 'choice', tip: 'JSON / CSV', choice: ['JSON', 'CSV'], value: 'JSON'},
+        datapath: {name: 'datapath', label: 'Path to Collection', type: 'text', tip: 'A path in dot notation.', value: ''}
       };
 
       scope.kind = 'source';
       scope.get = function(url){
 
         var preprocessor = L.identity;
+        if(scope.settings.datapath.value !== ''){
+          preprocessor = preprocessor.then(L.deep_property(utils.undotted(scope.settings.datapath.value)));
+        }
         if(scope.settings.mapping.value !== ''){
           var mapping = JSON.parse(scope.settings.mapping.value)
-          preprocessor = preprocessor.then(utils.extractor(mapping))
+          preprocessor = preprocessor.then(utils.extractor(mapping).map(_.map))
+        }
+        else{
+          preprocessor = preprocessor.then(L.identity.map(_.map))
         }
 
         $http.get(url).success(function(data, status, headers, config){
           if(scope.settings.format.value === 'csv' || (!scope.settings.format && url.endsWith('csv'))){
             data = $.csv.toObjects(data);
           }
-          _.each(data, preprocessor.get);
+          data = preprocessor.get(data);
           scope.globals.fill_with(data);
         }).error(function(data, status, headers, config){
           $window.alert('Error: ' + status);
