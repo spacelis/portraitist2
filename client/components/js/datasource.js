3c8f0b3372a8fbf5d7ca26dff4a260b451305859
wenli <spacelis@gmail.com>
2014-08-21 14:58:41 +0200

    Improved datasource component

--- client/components/js/datasource.js
+++ client/components/js/datasource.js
@@ -1,33 +1,45 @@
 /* global define */
 
-define(['resource_ctrl', 'crossfilter', 'underscore', 'utils', 'jQuery', 'csv'], function(ResourceCtrl, crossfilter, _, utils, $){
+define(['resource_ctrl', 'crossfilter', 'underscore', 'utils', 'lenz', 'jQuery', 'csv'], function(ResourceCtrl, crossfilter, _, utils, L, $){
   console.log('construct directive APIENDPOINT');
   return ResourceCtrl.directive('datasource', function($http, $window){
     console.log('init directive APIENDPOINT');
     // <apiendpoint id='endpoint1' data-url='https://' data-parsetime='a,b,c' data-format='header1,header2'></apiendpoint>
+    //
+
+
     function link(scope, element, attr){
       scope.widget_id = attr.id;
-      var $globals = scope.$parent.globals;
+      scope.widget_name = attr.name
+      scope.globals = scope.$parent.globals;
       scope.url = attr.url;
-      scope.format = attr.format;
-      scope.parsetime = attr.parsetime;
-      scope.widget_id = element.attr('id');
-      var preprocessor = utils.transformer(_.object(_.map(scope.parsetime.split(','), function(d){return [d, function(x){return new Date(x);}];})));
+      scope.settings = {
+        mapping: {name: 'mapping', label: 'Field Mapping', type: 'text', tip: 'A JSON string of object mapping the data.', value: '' }, 
+        datetime: {name: 'datetime', label: 'Datetime Fields', type: 'text', tip: 'The fields that is of type datetime.', value: ''},
+        format: {name: 'format', label: 'Format', type: 'choice', tip: 'JSON / CSV', choice: ['JSON', 'CSV'], value: 'JSON'},
+      };
 
       scope.kind = 'source';
       scope.get = function(url){
+
+        var preprocessor = L.identity;
+        if(scope.settings.mapping.value !== ''){
+          var mapping = JSON.parse(scope.settings.mapping.value)
+          preprocessor = preprocessor.then(utils.extractor(mapping))
+        }
+
         $http.get(url).success(function(data, status, headers, config){
-          if(scope.format === 'csv' || (!scope.format && url.endsWith('csv'))){
+          if(scope.settings.format.value === 'csv' || (!scope.settings.format && url.endsWith('csv'))){
             data = $.csv.toObjects(data);
           }
-          _.each(data, preprocessor);
-          $globals.fill_with(data);
+          _.each(data, preprocessor.get);
+          scope.globals.fill_with(data);
         }).error(function(data, status, headers, config){
           $window.alert('Error: ' + status);
         });
       };
       scope.config = function(){};
-      $globals.add_source(scope.widget_id, scope);
+      scope.globals.add_source(scope.widget_id, scope);
     }
     return {
       scope: true,
